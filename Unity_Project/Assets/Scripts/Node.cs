63a869bb3d5c0ae1730fcf4add123f76cad9c336
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-12-06 15:49:47 -0500

    add Node.cs script (empty)

---
+++ Unity_Project/Assets/Scripts/Node.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    string userID;
+    string handle;
+    bool verified;
+    List<string> followingList;
+    int followingCount;
+    int followerCount;
+
+}
