e073fd60c9f67540ca03b4777de6f2fa67f9e457
crc1225 <qx56jx35@gmail.com>
2021-12-06 23:31:05 -0500

    Mostly Complete TwitterService

---
+++ Script/Services/FileIOService.cs
@@ -0,0 +1,50 @@
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using Models;
+
+namespace Sercices
+{
+    class FileIOService
+    {
+        private static string MakeCSVLine(TwitterUser user)
+        {
+            var builder = new StringBuilder();
+            
+            builder.Append(user.Id);
+            builder.Append(",");
+            builder.Append(user.Username);
+            builder.Append(",");
+            builder.Append(user.Name);
+            builder.Append(",");
+            builder.Append(user.Verified);
+            builder.Append(",");
+
+            builder.Append(user.PublicMetrics.Followers);
+            builder.Append(",");
+            builder.Append(user.PublicMetrics.Following);
+            builder.Append(",");
+            builder.Append(user.PublicMetrics.Tweets);
+            builder.Append(",");
+            builder.Append(user.PublicMetrics.Listed);
+
+            return builder.ToString();
+        }
+
+        public static void ToCSV(TwitterUser rootUser, IEnumerable<TwitterUser> users)
+        {
+            List<string> lines = new List<string>();
+
+            lines.Add(MakeCSVLine(rootUser));
+            
+            foreach (var user in users)
+            {
+                lines.Add(MakeCSVLine(user));
+            }
+
+            File.WriteAllLines($"output/{rootUser.Username}.csv", lines);
+        }
+    }
+}
--- Script/Services/TwitterService.cs
+++ Script/Services/TwitterService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Linq;
@@ -25,7 +26,7 @@ namespace Sercices
             header.Add("Authorization", $"Bearer {token}");
             
             // Set the Http Base address to twitter.com
-            _client.BaseAddress = new Uri("https://api.twitter.com");
+            _client.BaseAddress = new Uri("https://api.twitter.com/2/");
 
             // Configure jsonOptions during request to ignore Upper/Lowercase
             if ( _jsonOptions is null )
@@ -37,47 +38,78 @@ namespace Sercices
             }
         }
 
-        public void GetUserByHandle(string handle)
+        public async Task<TwitterUser> GetUserByHandle(string handle)
         {
+            var response = await _client.GetAsync($"users/by/username/{handle}?user.fields=public_metrics,verified");
 
-        }
-
-        public void GetUserById(string id)
-        {
-
-        }
-
-        public void GetFollowersById(ulong id)
-        {
-            
-        }
-
-        public async Task<FollowingResponse> GetFollowingById(string id, string pageToken = "")
-        {
-            Console.WriteLine($"Getting Following info for {id} with page {pageToken}");
-            
-            HttpResponseMessage response;
-            if (pageToken.Length > 1)
-                response = await _client.GetAsync($"2/users/{id}/following?max_results=1000&user.fields=public_metrics,verified&pagination_token={pageToken}");
-            else
-                response = await _client.GetAsync($"2/users/{id}/following?max_results=1000&user.fields=public_metrics,verified");
+            PrintRateLimit(response.Headers, "GetUserByHandle Ratelimit: ");
 
             if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
             {
-                // Handle Rate Limit
+                await WaitForRateLimit(response.Headers);
+
+                response = await _client.GetAsync($"users/by/username/{handle}?user.fields=public_metrics,verified");
             }
 
             response.EnsureSuccessStatusCode();
 
             var stream = await response.Content.ReadAsStreamAsync();
+            var result = await JsonSerializer.DeserializeAsync<UserResponse>(stream, _jsonOptions);
 
+            return result.data;
+        }
+
+        public async Task<TwitterUser> GetUserById(string id)
+        {
+            var response = await _client.GetAsync($"users/{id}?user.fields=public_metrics,verified");
+            
+            PrintRateLimit(response.Headers, "GetUserById Ratelimit: ");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+            {
+                await WaitForRateLimit(response.Headers);
+
+                response = await _client.GetAsync($"users/{id}?user.fields=public_metrics,verified");
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStreamAsync();
+            var result = await JsonSerializer.DeserializeAsync<UserResponse>(stream, _jsonOptions);
+
+            return result.data;
+        }
+
+        public async Task<FollowingResponse> GetFollowingByUser(TwitterUser user, string pageToken = "")
+        {
+            Console.WriteLine($"Getting Following info for {user.Username} with page {pageToken}");
+            
+            HttpResponseMessage response;
+            if (pageToken.Length > 1)
+                response = await _client.GetAsync($"users/{user.Id}/following?max_results=1000&user.fields=public_metrics,verified&pagination_token={pageToken}");
+            else
+                response = await _client.GetAsync($"users/{user.Id}/following?max_results=1000&user.fields=public_metrics,verified");
+
+            PrintRateLimit(response.Headers, "GetFollowingByUser Ratelimit: ");
+            
+            if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+            {
+                await WaitForRateLimit(response.Headers);
+
+                if (pageToken.Length > 1)
+                    response = await _client.GetAsync($"users/{user.Id}/following?max_results=1000&user.fields=public_metrics,verified&pagination_token={pageToken}");
+                else
+                    response = await _client.GetAsync($"users/{user.Id}/following?max_results=1000&user.fields=public_metrics,verified");
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStreamAsync();
             var result = await JsonSerializer.DeserializeAsync<FollowingResponse>(stream, _jsonOptions);
 
             if (result.meta.NextToken is not null)
             {
-                var moreResult = await GetFollowingById(id, result.meta.NextToken);
-
-                Console.WriteLine(moreResult.data.Count());
+                var moreResult = await GetFollowingByUser(user, result.meta.NextToken);
 
                 result.data = result.data.Concat(moreResult.data);
             }
@@ -85,9 +117,32 @@ namespace Sercices
             return result;
         }
 
-        public void GetIdByHandle(string handle)
+        private async Task WaitForRateLimit(HttpResponseHeaders headers)
         {
+            var resetLimit = headers.First(x => x.Key == "x-rate-limit-reset").Value.ElementAt(0);
 
+            var futureTime = new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(Convert.ToDouble(resetLimit));
+            var time = futureTime - DateTime.UtcNow;
+
+            while (time.Milliseconds > 0)
+            {
+                Console.WriteLine($"We are currently Rate Limited!\nWaiting for {time.Minutes + 1} minutes...");
+                
+                await Task.Delay(60000);
+
+                resetLimit = headers.First(x => x.Key == "x-rate-limit-reset").Value.ElementAt(0);
+
+                futureTime = new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(Convert.ToDouble(resetLimit));
+                time = futureTime - DateTime.UtcNow;
+            }
+        }
+
+        private void PrintRateLimit(HttpResponseHeaders headers, string type)
+        {
+            var limit = headers.First(x => x.Key == "x-rate-limit-limit").Value.ElementAt(0);
+            var remaining = headers.First(x => x.Key == "x-rate-limit-remaining").Value.ElementAt(0);
+            var reset = headers.First(x => x.Key == "x-rate-limit-reset").Value.ElementAt(0);
+            Console.WriteLine(String.Join(", ", type + limit, remaining, reset));
         }
     }
 }
