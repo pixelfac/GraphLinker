2caa61b0296c0821fab819cf1ce2b5de0de89dfe
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-12-08 23:27:42 -0500

    change Bellman-Ford to BFS

--- Unity_Project/Assets/Scripts/Pathfinder.cs
+++ Unity_Project/Assets/Scripts/Pathfinder.cs
@@ -8,8 +8,8 @@ public static class Pathfinder
 	{
 		throw new NotImplementedException("Dikstra's Not Yet Implemented");
 	}
-	public static int BellmanFord(string node1, string node2)
+	public static int BFS(string node1, string node2)
 	{
-		throw new NotImplementedException("Bellman-Ford's Not Yet Implemented");
+		throw new NotImplementedException("BFS Not Yet Implemented");
 	}
 }
