2f37cce031cae02e20b41f44089bd0e34649a503
crc1225 <qx56jx35@gmail.com>
2021-12-08 22:42:20 -0500

    Update Script

--- Script/Program.cs
+++ Script/Program.cs
@@ -23,24 +23,40 @@ namespace Script
 
             //FileIOService.ToCSV(tommy, response.data);
 
-            var lines = 0;
-            foreach (var file in Directory.GetFiles("output"))
-            {
-                lines += (await File.ReadAllLinesAsync(file)).Count();
-            }
-
-            Console.WriteLine(lines);
-
-            // foreach (var creator in await File.ReadAllLinesAsync("creators.txt"))
+            // var lines = 0;
+            // foreach (var file in Directory.GetFiles("output"))
             // {
-            //     TwitterUser user = await service.GetUserByHandle(creator);
-            //     FollowingResponse response = await service.GetFollowingByUser(user);
-
-            //     if (user.PublicMetrics.Following != response.data.Count())
-            //        Console.WriteLine("Error: Following Count is not equal to data size");
-
-            //     FileIOService.ToCSV(user, response.data);
+            //     lines += (await File.ReadAllLinesAsync(file)).Count();
             // }
+            // Console.WriteLine(lines);
+
+            foreach (var creator in await File.ReadAllLinesAsync("creators.txt"))
+            {
+                if (File.Exists($"output/{creator}.csv"))
+                    continue;
+                
+                TwitterUser user = await service.GetUserByHandle(creator);
+                FollowingResponse response = await service.GetFollowingByUser(user);
+
+                if (user.PublicMetrics.Following != response.data.Count())
+                   Console.WriteLine("Error: Following Count is not equal to data size");
+
+                foreach (var depth in response.data.Where(x => x.Verified is true))
+                {
+                    if (File.Exists($"output/{depth.Username}.csv"))
+                        continue;
+                    
+                    TwitterUser newUser = await service.GetUserByHandle(depth.Username);
+                    FollowingResponse res = await service.GetFollowingByUser(newUser);
+
+                    if (user.PublicMetrics.Following != response.data.Count())
+                        Console.WriteLine("Error: Following Count is not equal to data size");
+
+                    FileIOService.ToCSV(newUser, res.data);
+                }
+
+                FileIOService.ToCSV(user, response.data);
+            }
         }
     }
 }
