aa7bd242a1a7a7795cdffbfa9ae3dc8503fbce7e
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-12-08 22:48:20 -0500

    make Pathfinder template class

---
+++ Unity_Project/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public static class Pathfinder
+{
+    public static int Dijkstra(string node1, string  node2)
+	{
+		throw new NotImplementedException("Dikstra's Not Yet Implemented");
+	}
+	public static int BellmanFord(string node1, string node2)
+	{
+		throw new NotImplementedException("Bellman-Ford's Not Yet Implemented");
+	}
+}
